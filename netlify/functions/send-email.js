cee179705d1de8fda873b2cdf72ebd0724dc005f
ratzrahul-bit <ratzrahul@gmail.com>
2026-06-24 12:03:06 +0530

    Update email sender details in send-email function

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -2,10 +2,8 @@ exports.handler = async function(event) {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method not allowed' }
   }
-
   const BREVO_API_KEY = process.env.BREVO_API_KEY
   const { to, toName, subject, html } = JSON.parse(event.body)
-
   const response = await fetch('https://api.brevo.com/v3/smtp/email', {
     method: 'POST',
     headers: {
@@ -13,13 +11,12 @@ exports.handler = async function(event) {
       'api-key': BREVO_API_KEY,
     },
     body: JSON.stringify({
-      sender: { name: 'IGM Nepal', email: 'manifestnepal@gmail.com' },
+      sender: { name: 'Himalayan Manifest', email: 'himalayanmanifest@gmail.com' },
       to: [{ email: to, name: toName }],
       subject,
       htmlContent: html,
     }),
   })
-
   const data = await response.json()
   return {
     statusCode: response.ok ? 200 : 400,
